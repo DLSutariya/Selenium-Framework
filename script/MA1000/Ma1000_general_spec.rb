3ddba966893d15fbebd4d5d960e28f176618a543
DilipSutariya <1992dilip@gmail.com>
2015-10-12 19:49:22 +0530

    New test

--- script/MA1000/Ma1000_general_spec.rb
+++ script/MA1000/Ma1000_general_spec.rb
@@ -1,14 +1,21 @@
 require 'rspec'
 require 'selenium-webdriver'
-require_relative '../../lib/base_test'
+
 
 
 describe 'Terminal Information' do
 
   before(:each) do
     @driver = Selenium::WebDriver.for :firefox
+
+    # maximize window screen
+    @driver.manage.window.maximize
+
+    # URL for the Webserver
     @base_url = "http://10.102.3.146/"
+
     @accept_next_alert = true
+
     @driver.manage.timeouts.implicit_wait = 30
   end
 
@@ -16,12 +23,41 @@ describe 'Terminal Information' do
     @driver.quit
   end
 
-  it 'should check terminal discription' do
+  it 'Check Valid Webserver Tital page valid name' do
 
     @driver.get(@base_url + "/")
+
+    #clear password field using Id
     @driver.find_element(:id, "password").clear
+
+    #send String to password field usig Id
     @driver.find_element(:id, "password").send_keys "12345"
+
+    # Click on login button using Xpath
     @driver.find_element(:xpath, "//*[@id='loginform']/table/tbody/tr[4]/td/input").click
+    stringTitle = "Safran Morpho"
+
+    puts stringTitle.should match(@driver.title)
 
   end
+
+  it 'Check InValid Webserver Tital page valid name' do
+
+    @driver.get(@base_url + "/")
+
+    #clear password field using Id
+    @driver.find_element(:id, "password").clear
+
+    #send String to password field usig Id
+    @driver.find_element(:id, "password").send_keys "12345"
+
+    # Click on login button using Xpath
+    @driver.find_element(:xpath, "//*[@id='loginform']/table/tbody/tr[4]/td/input").click
+    stringTitle = "test"
+
+    puts stringTitle.should_not match(@driver.title)
+
+  end
+
+
 end
